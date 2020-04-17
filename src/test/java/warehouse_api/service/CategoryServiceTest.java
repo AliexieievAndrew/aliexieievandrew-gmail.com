d5995a592f79dc647116352a060a1831d76b8f8b
Andrey Andrey <Andrew.Aliexieiev@tbibank.bg>
2020-04-17 19:27:22 +0300

    added fixtures, configured test db

--- src/test/java/warehouse_api/service/CategoryServiceTest.java
+++ src/test/java/warehouse_api/service/CategoryServiceTest.java
@@ -1,14 +1,17 @@
 package warehouse_api.service;
 
+import org.junit.Assert;
 import org.junit.Test;
 import warehouse_api.config.BaseTest;
 import warehouse_api.model.entity.Category;
 import warehouse_api.repository.CategoryDao;
 
+import javax.ejb.EJB;
 import javax.naming.NamingException;
 import java.util.List;
 
 public class CategoryServiceTest extends BaseTest {
+
 
     @Test
     public void testAll() throws NamingException {
@@ -20,4 +23,23 @@ public class CategoryServiceTest extends BaseTest {
         System.out.println("size:" + all.size());
         System.out.println("TEST");
     }
+
+    @Test
+    public void findAllTest() throws Exception {
+        CategoryService categoryService = (CategoryService) ctx.lookup("java:global/classes/CategoryService");
+        List<Category> all = categoryService.all();
+
+        Assert.assertTrue(all.size() > 0);
+    }
+//
+//    @Test
+//    public void saveTest() throws Exception {
+//        CategoryService categoryService = (CategoryService) ctx.lookup("java:global/classes/CategoryService");
+//
+//        Category category = new Category();
+//        category.setCategoryName("test");
+//        categoryService.save(category);
+//
+//        Assert.assertTrue(true);
+//    }
 }
